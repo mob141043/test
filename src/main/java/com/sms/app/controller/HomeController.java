80a7287787a5394e46e688d11c0f397c56af398a
sonvhh <mob5832@gmail.com>
2022-04-28 23:56:08 +0700

    ok

--- src/main/java/com/sms/app/controller/HomeController.java
+++ src/main/java/com/sms/app/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/home")
 public class HomeController {
 
-    @GetMapping("/hello-world")
+    @GetMapping()
     public ResponseEntity<String> home(){
         return new ResponseEntity<String>("<h1>OK</h1>",HttpStatus.OK) ;
     }
